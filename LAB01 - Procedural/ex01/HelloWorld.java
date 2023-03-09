c9f733b245471b269e5659e3f0e81dc0f110de18
Ana Bárbara Campos Conti <ana.conti@ufu.br>
2023-03-09 15:59:32 -0300

    .

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Ana Barbara");
+        System.out.println("12021BCC017");
+    }
+}
